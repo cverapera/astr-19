b16e0add308534272248a8f23919c0ca48cd6b02
cverapera <165837629+cverapera@users.noreply.github.com>
2025-10-19 14:14:26 -0700

    Add files via upload
    Session 2 Prompt, Coding Journal 2

---
+++ floating-numbers.py
@@ -0,0 +1,20 @@
+# Sum of two floating point numbers
+float_num1 = 5.7
+float_num2 = 3.3
+sum_result = float_num1 + float_num2
+print("Sum of floats:", sum_result)
+print("Data type:", type(sum_result))
+
+# Difference between two integers
+int_num1 = 10
+int_num2 = 4
+diff_result = int_num1 - int_num2
+print("Difference of integers:", diff_result)
+print("Data type:", type(diff_result))
+
+# Product of a floating point number and an integer
+float_num3 = 2.5
+int_num3 = 6
+product_result = float_num3 * int_num3
+print("Product of float and int:", product_result)
+print("Data type:", type(product_result))
