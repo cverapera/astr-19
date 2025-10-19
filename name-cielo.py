6db3438d91a479f2a7636335c0f4d41d84852eab
cverapera <165837629+cverapera@users.noreply.github.com>
2025-10-19 12:51:15 -0700

    Add files via upload
    Prompt 1 Coding Journal 1

---
+++ name-cielo.py
@@ -0,0 +1,21 @@
+#This program will write 
+# My name is Cielo I go by she and her.A few things I like is chicken and the movie UP
+
+
+# This function tells the
+# computer to pring 
+# My name is Cielo, I go by she and her.A few things I like is chicken and the movie UP!
+def PrintMynameisCieloIgobysheandherAfewthingsIlikeischickenandthemovieUP():
+		print ("My name is Cielo, I go by she and her.A few things I like is chicken and the movie UP!")
+
+
+#This defines our main()
+#function for our program
+def main():
+		PrintMynameisCieloIgobysheandherAfewthingsIlikeischickenandthemovieUP()
+
+
+#When we run the program,
+#this executes first
+if __name__=="__main__":
+		main()
